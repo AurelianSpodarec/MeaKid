39d778a91a3acaa7dd3c7212b82bfac373a7899e
Aurelian <aurelians@libero.it>
2020-02-13 23:57:11 +0000

    Updated name from Product to ProductList

--- src/views/Routes.js
+++ src/views/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomeView from './Home/HomeView';
-import ProductList from './Product/ProductList';
+import ProductList from './ProductList/ProductList';
 import ProductItem from './ProductItem/ProductItem';
 import NotFound from './NotFound/NotFound'
 
