d10a42f0619326816d9b31ca253611c225e5288c
Aurelian <aurelians@libero.it>
2020-02-13 23:53:10 +0000

    Added fake json products

---
+++ src/json/products/products.js
@@ -0,0 +1,12 @@
+[
+    {
+        "id": 1,
+        "title": "Nike Max Air 12",
+        "coverImage": "url",
+        "media": ["url"],
+        "price": {
+            "price": 10,
+            "currency": "USD",
+        }
+    }
+]
