a13f33d93e696ae917c3df3e52c4145ce9560044
Aurelian <aurelians@libero.it>
2020-02-13 22:36:51 +0000

    Initial commit

---
+++ src/views/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Header, Footer } from './../components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Layout() {
+    return (
+        <div>
+            <Header />
+
+
+            <h1>Template</h1>
+
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
